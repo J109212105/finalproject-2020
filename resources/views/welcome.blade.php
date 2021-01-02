7b1ce6841edca6a4978454f74d6706788f97d8dd
J109212105 <j109212105@nkust.edu.tw>
2021-01-02 16:27:04 +0800

    created 20210102

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,7 @@
                     </svg> -->
                     <dl>
                     <dt><marquee direction="right" behavior=alternate bgcolor="orange" width=195%>歡迎蒞臨林建成(J109212105)的期末網站</marquee>
-                    <dt><h1 style= "background:gainsboro"><font face="標楷體" color="blue">林建成(J109212105)的期末網站</font></h1>
+                    <dt><h1><font face="標楷體" color="blue" align="center" style="background-color:gainsboro">林建成(J109212105)的期末網站</font></h1>
                     
                     <hr>
                     <nav class="navbar navbar-expand-lg navbar-light bg-light">
