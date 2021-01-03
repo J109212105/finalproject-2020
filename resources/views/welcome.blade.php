fc44cea9d1b5a486c77294bbc06b1f5a2114ef04
J109212105 <j109212105@nkust.edu.tw>
2021-01-04 00:45:40 +0800

    created 20210103

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,6 +31,7 @@
         <div class="marquee">
            <h2> Paris</h2>
            <p>Paris is the capital of France.</p>
+        </div>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
