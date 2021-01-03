fe178681d7d399195c928f347288cc12ecfaafdd
J109212105 <j109212105@nkust.edu.tw>
2021-01-03 23:24:41 +0800

    created 20210103

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,13 +34,7 @@
         }
         </style>
     </head>
-
-    <body>
-        <h2 class="city">Paris</h2>
-        <p class="city">Paris is the capital of France.</p>
-
-    </body>
-
+  
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
@@ -56,6 +50,10 @@
                     @endif
                 </div>
             @endif
+            <div class="city">
+                 <h2> Paris</h2>
+                 <p>Paris is the capital of France.</p>
+            </div>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
