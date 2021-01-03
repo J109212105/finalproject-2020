d8df04f77375d5ffe2c4cc759daf3664b35f0179
J109212105 <j109212105@nkust.edu.tw>
2021-01-04 00:06:16 +0800

    created 20210103

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,11 +37,7 @@
         }
         </style>
     </head>
-    <body>
-        <h2> Paris</h2>
-        <p>Paris is the capital of France.</p>
-    </body>
-  
+    
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
             @if (Route::has('login'))
@@ -57,8 +53,11 @@
                     @endif
                 </div>
             @endif
-
-            <div class="max-w-6xl mx-auto sm:px-16 lg:px-8">
+                <div class="hidden fixed top-10 left-0 px-6 py-4 sm:block">
+                    <h2> Paris</h2>
+                    <p>Paris is the capital of France.</p>
+                </div>
+            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <!-- <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto text-gray-700 sm:h-20">
                         <g clip-path="url(#clip0)" fill="#EF3B2D">
