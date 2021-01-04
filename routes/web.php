520bdc4563964b919039bdd6a4db3e53facc8732
J109212105 <j109212105@nkust.edu.tw>
2021-01-04 21:06:42 +0800

    changed 20210104

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayListController;
+use App\Http\Controllers\MainController;
+
+Route::get('/', [MainController::class, "index"]);
+Route::post('/insert/', [MainController::class, "insert"]);
+Route::get('/mylogout/', [MainController::class, "mylogout"]);
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -30,3 +25,4 @@ Route::get('/lucky/', function () {
     }
     return view('lotto', compact('lucky_number', 'numbers'));
 });
+*/
